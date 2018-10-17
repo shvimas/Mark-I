8f3bb5440a2aeb26429bbdcfb11f4b65c71e59a6
shvimas <shvimerd@gmail.com>
2018-10-17 22:21:27 +0300

    remove redundant git pulls

--- scripts/docker-build-crawler.sh
+++ scripts/docker-build-crawler.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-git pull && docker build -f docker/crawler_Dockerfile -t crawler . ||
+docker build -f docker/crawler_Dockerfile -t crawler . ||
     echo -e "\033[0;31m Failed to build crawler; is crawler-base image built? \033[0m"
