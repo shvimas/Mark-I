76eafd9746e4f640b53ac7df7ea6b3eb58b4b558
shvimas <shvimerd@gmail.com>
2018-10-20 17:31:44 +0300

    remove old inconsistent message

--- scripts/docker-run-crawler.sh
+++ scripts/docker-run-crawler.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-docker run -d --name=crawler --network crawler-network --env-file envs/crawler-env.list crawler ||
-    echo -e "\033[0;31m Failed to start crawler; maybe MongoDB container is not up? \033[0m"
+docker run -d --name=crawler --network crawler-network --env-file envs/crawler-env.list crawler
